51e39567c5297ddac5553444ee836bb1851b4442
Dan Roberts <dan@adadevelopersacademy.org>
2018-05-16 09:47:56 -0700

    Get some basic model tests up and running

--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
-class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Pet do
+  describe "validations" do
+    let (:pet_data) {{name: 'socks', age: 5, owner: 'bill'}}
+    it "Can be created with all values" do
+      pet = Pet.new(pet_data)
+      pet.must_be :valid?
+    end
+
+    it "Requires a name" do
+      pet_data[:name] = nil
+      pet = Pet.new(pet_data)
+      pet.wont_be :valid?
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/rails'
+
+#  For colorful output!
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
