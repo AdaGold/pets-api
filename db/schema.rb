1b3cbb4eba7f509d19aa3ea38550327d2648a3bd
Kaida Masaki <kaida@adadevelopersacademy.org>
2020-02-24 16:09:18 -0800

    Upgrade to Rails 6

--- db/schema.rb
+++ db/schema.rb
@@ -2,26 +2,26 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609194446) do
+ActiveRecord::Schema.define(version: 2017_06_09_194446) do
 
   create_table "pets", force: :cascade do |t|
-    t.string   "name"
-    t.string   "img"
-    t.string   "breed"
-    t.integer  "age"
-    t.string   "about"
-    t.string   "owner"
+    t.string "name"
+    t.string "img"
+    t.string "breed"
+    t.integer "age"
+    t.string "about"
+    t.string "owner"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean  "vaccinated"
+    t.boolean "vaccinated"
   end
 
 end
